5dd81ac233d84515fe014d5e3862ef6986679f7c
Prashantsingh1234 <your.email@example.com>
2024-07-15 13:31:31 +0530

    to_do_app_react_js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Keys } from "./Keys";
+import { Todo } from "./projects/Todo/Todo";
+//import { TodoForm } from "./projects/Todo/TodoForm";
+
+export const App = () => {
+  return (
+  // <Keys />
+  <>
+  <Todo/>
+  </>
+);
+};
